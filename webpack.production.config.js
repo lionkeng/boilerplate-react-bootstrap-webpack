422f669e1f18ce6e9fc0ac43ffffd12acdd6ed5c
keng <keng@renderinghouse.com>
2015-06-05 12:56:09 +0800

    implemented what is webpack -p all content, assets goes into a folder named dist. image-webpack-loader just dumps everything into the current folder.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -6,7 +6,7 @@ var child_process = require('child_process');
 
 // from Pete Hunt's webpack-howto
 var definePlugin = new webpack.DefinePlugin({
-  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
+  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'false')),
   __PRERELEASE__: JSON.stringify(JSON.parse(process.env.BUILD_PRERELEASE || 'false'))
 });
 
@@ -16,27 +16,39 @@ var commonLoaders = [
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /.*\.(gif|png|jpg)$/, loaders: ['file?hash=sha512&digest=hex&size=16&name=[hash].[ext]', 'image-webpack-loader?optimizationLevel=7&interlaced=false']},
       { test: /.*\.(eot|woff|ttf|svg)/, loader: 'file?hash=sha512&digest=hex&size=16&name=cd [hash].[ext]'},
-      { test: /\.jsx?$/, loaders: ['babel-loader'], exclude: /node_modules/},
-      // { test: /\.js$/, loader: 'jsx-loader?harmony' }
+      { test: /\.jsx?$/, loaders: ['babel-loader']},
+      //{ test: /\.js$/, loader: 'jsx-loader?harmony' }
 ]; 
 
 var basicExtensions = ['', '.js', '.json', '.jsx'];
 
-child_process.exec('cp app/index.dev.html app/index.html', function(error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    }
+
+function copyIndexFile() {
+  child_process.exec('cp app/index.production.html dist/index.html', function(error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+  });
+}
+
+child_process.exec('mkdir dist', function(error, stdout, stderr) {
+  copyIndexFile();
 });
+
 
 var config = {
   entry: {
-    entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'app/jsx/main.jsx')]
+    entry: [path.resolve(__dirname, 'app/jsx/main.jsx')],
+    // Since react is installed as a node module, node_modules/react,
+    // we can point to it directly, just like require('react');
+    vendors: ['react', 'react-bootstrap']
+
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'       
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'       
   },
   resolve: {
     extensions: basicExtensions
@@ -44,7 +56,8 @@ var config = {
   module: {
     loaders: commonLoaders
   },
-  plugins: [definePlugin],
+  plugins: [definePlugin,
+            new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')],
 
 };
 
